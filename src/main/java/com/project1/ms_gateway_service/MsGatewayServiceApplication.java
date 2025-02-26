d81169db5df76d1635a0d6b6a4cac9d04db832b8
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-26 15:41:21 -0500

    first commit

---
+++ src/main/java/com/project1/ms_gateway_service/MsGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project1.ms_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayServiceApplication.class, args);
+	}
+
+}
